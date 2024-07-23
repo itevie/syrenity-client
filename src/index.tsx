b596b55911b119d57345933eb8f35532e4f541dd
itevie <izzyplays07@gmail.com>
2024-07-23 22:30:16 +0100

    stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './MainApp/App';
 
+import "./styles/base.css";
+import "./styles/context-menus.css";
+import "./styles/elements.css";
+import "./styles/message.css";
+import "./styles/modals.css";
+import "./styles/pages.css";
+import "./styles/routes.css";
+import "./styles/tooltip.css";
+
+import ModalManager from './components/ModalManager';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './routes/Login';
+import ContextMenuManager from './components/ContextMenuManager';
+import Home from './routes/Home';
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+    // ----- Main App Shit -----
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/channels",
+        element: <App />
+    },
+    {
+        path: "/channels/:channelId?/:guildId?",
+        element: <App />
+    },
+
+    // ----- Other Site -----
+
+    // ----- User Management -----
+    {
+        path: "/login",
+        element: <Login />
+    }
+]);
+
+root.render(
+    <>
+        <RouterProvider router={router} />
+        <ModalManager />
+        <ContextMenuManager />
+    </>
+);
